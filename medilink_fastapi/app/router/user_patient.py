ba3d2cc018ff6888afbd2ec4210a791075ed1894
Rohit <rohitkale523@gmail.com>
2024-07-30 05:17:41 +0530

    Signup patient is using that fake db auth now create login patient which takes bearer token from signup and uses it to authenticate

--- medilink_fastapi/app/router/user_patient.py
+++ medilink_fastapi/app/router/user_patient.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from ..database import SessionLocal
 from fastapi import APIRouter
 from .. import schemas, crud
+from ..auth import get_current_user
+from ..models import Patient
 
 
 router = APIRouter(tags=["Patient"],)
@@ -17,8 +19,9 @@ def get_db():
         db.close()
 
 @router.post("/signup_patient/", response_model=schemas.Patient)
-def create_patient(patient: schemas.Patient, db: Session = Depends(get_db)):
+def create_patient(patient: schemas.Patient, db: Session = Depends(get_db), current_user: Patient = Depends(get_current_user)):
     # db_user = crud.get_user_by_email(db, email=user.email)
     # if db_user:
     #     raise HTTPException(status_code=400, detail="Email already registered")
+    print(current_user)
     return crud.create_patient(db=db, patient=patient)
